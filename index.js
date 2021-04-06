f64e3102cde222dec6ad159b0d55f91e4e4bdc76
a-rolland <antoine-rolland@hotmail.fr>
2021-04-06 18:06:33 +0200

    insideOut words

--- index.js
+++ index.js
@@ -1,85 +1,27 @@
-// Giving the following arrays of objects, let’s filter just the places with a pool.
+function insideOut(x) {
+  const processArray = (array, a, b) =>
+    array.split("").splice(a, b).reverse().join("");
+  return x
+    .split(" ")
+    .map((word) => {
+      return word.length <= 3
+        ? word
+        : word.length % 2 === 0
+        ? [
+            processArray(word, 0, word.length / 2),
+            processArray(word, word.length - word.length / 2, word.length),
+          ].join("")
+        : word.length % 2 !== 0
+        ? [
+            processArray(word, 0, word.length / 2),
+            word[Math.floor(word.length / 2)],
+            processArray(word, word.length - word.length / 2 + 1, word.length),
+          ].join("")
+        : 0;
+    })
+    .join(" ");
+}
 
-const places = [
-  {
-   title: "Awesome Suite 20' away from la Rambla",
-   price: 200,
-   type: "Private Room",
-   pool: true,
-   garage: false
-  },
-  {
-   title: "Private apartment",
-   price: 190,
-   type: "Entire Place",
-   pool: true,
-   garage: true
-  },
-  {
-   title: "Apartment with awesome views",
-   price: 400,
-   type: "Entire Place",
-   pool: false,
-   garage: false
-  },
-  {
-   title: "Apartment in la Rambla",
-   price: 150,
-   type: "Private Room",
-   pool: false,
-   garage: true
-  },
-  {
-   title: "Comfortable place in Barcelona´s center",
-   price: 390,
-   type: "Entire place",
-   pool: true,
-   garage: true
-  },
-  {
-   title: "Room near Sagrada Familia",
-   price: 170,
-   type: "Private Room",
-   pool: false,
-   garage: false
-  },
-  {
-   title: "Great house next to Camp Nou",
-   price: 140,
-   type: "Entire place",
-   pool: true,
-   garage: true
-  },
-  {
-   title: "New apartment with 2 beds",
-   price: 2000,
-   type: "Entire place",
-   pool: false,
-   garage: true
-  },
-  {
-   title: "Awesome Suite",
-   price: 230,
-   type: "Private Room",
-   pool: false,
-   garage: false
-  },
-  {
-   title: "Apartment 10' from la Rambla",
-   price: 930,
-   type: "Entire place",
-   pool: true,
-   garage: true
-  }
- ];
- 
- //forma 1
- //places
- function compare(a,b) {
-   return a.price-b.price
- }
- 
- //places.sort(compare);
- 
- //forma 2
- console.log(places.sort((a,b) => a.price - b.price))
+const word = "what time are we climbing up the volcano";
+
+console.log(insideOut(word));
